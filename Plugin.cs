7223eafca4111db5b884a3045b92a8b189aeb5ed
Steffan Donal <steffan@sdonalcreative.uk>
2018-08-25 17:22:15 +0200

    Semver!

--- Plugin.cs
+++ Plugin.cs
@@ -17,7 +17,7 @@ namespace CustomColors
         bool _overrideCustomSabers = true;
 
         public string Name => "CustomColors";
-        public string Version => "1.1";
+        public string Version => "1.1.0";
 
         bool _init;
         bool _colorInit;

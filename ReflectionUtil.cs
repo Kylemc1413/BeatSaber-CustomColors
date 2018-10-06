37d906540f6604989e77aa66696cfd72937e3d1a
Kyle1413 <kylet1412@gmail.com>
2018-10-06 13:21:32 -0400

    Add Settings menu for Adjusting Modprefs Values

--- ReflectionUtil.cs
+++ ReflectionUtil.cs
@@ -1,17 +1,20 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
-
+using System.Text;
+using UnityEngine;
 namespace CustomColors
 {
-    internal class ReflectionUtil
+    internal static class ReflectionUtil
     {
-        public static void SetPrivateField(object obj, string fieldName, object value)
+        public static void SetPrivateField(this object obj, string fieldName, object value)
         {
             var field = obj.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
             field.SetValue(obj, value);
         }
 
-        public static T GetPrivateField<T>(object obj, string fieldName)
+        public static T GetPrivateField<T>(this object obj, string fieldName)
         {
             var field = obj.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
             var value = field.GetValue(obj);
@@ -25,10 +28,33 @@ namespace CustomColors
             return value;
         }
 
-        public static void InvokePrivateMethod(object obj, string methodName, object[] methodParams)
+        public static void InvokePrivateMethod(this object obj, string methodName, object[] methodParams)
         {
             var method = obj.GetType().GetMethod(methodName, BindingFlags.Instance | BindingFlags.NonPublic);
             method.Invoke(obj, methodParams);
+        }
+        public static Component CopyComponent(Component original, Type originalType, Type overridingType, GameObject destination)
+        {
+            var copy = destination.AddComponent(overridingType);
+
+            Type type = originalType;
+            while (type != typeof(MonoBehaviour))
+            {
+                CopyForType(type, original, copy);
+                type = type.BaseType;
+            }
+
+            return copy;
+        }
+
+        private static void CopyForType(Type type, Component source, Component destination)
+        {
+            FieldInfo[] myObjectFields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetField);
+
+            foreach (FieldInfo fi in myObjectFields)
+            {
+                fi.SetValue(destination, fi.GetValue(source));
+            }
         }
     }
 }
